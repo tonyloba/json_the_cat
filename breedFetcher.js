97fc4dbeb6b38425f550c0f1a4b28581812b1648
tonyloba <tonyloba@gmail.com>
2021-09-23 23:26:59 +0000

    updated files and add test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,27 +1,56 @@
 const request = require('request');
 //var fs = require('fs');
-
-// const getCatsUrl = `https://api.thecatapi.com/v1/images/search?q=${process.argv[2]}`;
-const getCatsUrl = `https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]}`;
+// const getCatsBreed = `https://api.thecatapi.com/v1/images/search?q=${process.argv[2]}`;
 
 
-request(getCatsUrl, function (error, response, body) {
-  if (error){
-    console.error('error:', error);
-  }  
-  const data = JSON.parse(body);
+
+// refactoring:
+const fetchBreedDescription = function(breedName, callback) {
   
+  const getCatsBreed = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+request(getCatsBreed, function (error, response, body) {
+  if (error){   
+    return callback(error,null);
+  }   
+
+  const data = JSON.parse(body);
+  //console.log(data)
   console.log(typeof data);
   if(data[0]){
-    console.log(data[0].description)
-  } else {
-    console.log("Nothing found")
-  }
-  
+    return callback(null,data[0].description)
+   } 
+  else {
+   
+    return callback("Nothing found",null)
+  } 
+  });
 
-});
+
+};
+
+module.exports = {fetchBreedDescription};
+
+
+
+
+// request(getCatsUrl, function (error, response, body) {
+//   if (error){
+//     console.error('error:', error);
+//   }  
+//   const data = JSON.parse(body);
+//   //console.log(data)
+//   console.log(typeof data);
+//   if(data[0]){
+//     console.log(data[0].description)
+//   } else {
+//     console.log("Nothing found")
+//   }  
+
+// });
+
 
 //   downloads  to the file path we specify:
 // .pipe(fs.createWriteStream(filePath));
-
+//
 
